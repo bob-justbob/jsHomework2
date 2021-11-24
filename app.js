8786ff349c79c4cb4fa7107f06a23d50a7d56f5d
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-11-24 16:49:40 +0400

    dide homewrok

---
+++ app.js
@@ -0,0 +1,16 @@
+let array = [14, 2, 45, 13, 54, 66, 21, 40];
+
+console.log(Math.max(...array))
+console.log(Math.min(...array))
+
+
+function multiplyNumbers(x,y){
+    return x*y;
+}
+
+let z = multiplyNumbers(2,3)
+console.log(z)
+
+const x = 2 ;
+let y = Math.pow(x, 3);
+console.log(y)
