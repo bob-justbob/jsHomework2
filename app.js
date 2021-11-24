5a40e5d2cac61550a044de7e36a28fd399466af2
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-11-25 01:32:46 +0400

    changedfanction

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-let array = [14, 2, 45, 13, 54, 66, 21, 40];
+// let array = [14, 2, 45, 13, 54, 66, 21, 40];
 
-console.log(Math.max(...array))
-console.log(Math.min(...array))
+// console.log(Math.max(...array))
+// console.log(Math.min(...array))
 
 
-function multiplyNumbers(x,y){
-    return x*y;
-}
 
-let z = multiplyNumbers(2,3)
-console.log(z)
-
-const x = 2 ;
-let y = Math.pow(x, 3);
-console.log(y)
+// const x = 4 ;
+// let y = Math.pow(x, 2);
+// console.log(y)
